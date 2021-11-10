5befb8b70f8248a271fbe45cb4117a897d156239
Iberê <ibere82@gmail.com>
2021-11-10 13:42:26 -0300

    state and store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,3 +3,11 @@
 
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
+
+function reducer(state, action) {
+  console.log(action);
+  console.log(state);
+  return state;
+}
+
+export default reducer;
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,7 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+
+function wallet(state) {
+  return state;
+}
+
+export default wallet;
